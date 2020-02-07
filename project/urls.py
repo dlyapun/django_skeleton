24c6cb697c67587e9161ae3b1e25afcf96db06c2
dlyapun <dlyapun@gmail.com>
2020-02-07 15:45:02 +0200

    Initial js-game

--- project/urls.py
+++ project/urls.py
@@ -9,6 +9,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="index.html")),
+    path('game', TemplateView.as_view(template_name="game.html")),
     path('accounts/', include('accounts.urls')),
     path('', include('core.urls')),
 ]
